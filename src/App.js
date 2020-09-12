476ac05bed756270d5ebff0d53ced3cd5a6dd6d4
farhanmaneri <farhanmaneri@gmail.com>
2020-09-11 22:21:14 -0700

    fileUpdated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import Navbar from './Navbar'
-import Navigation from './Navigation'
-
-
+import Navigation from './components/Navigation'
+import Carousls from './components/Carosuel'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MyNavbar from './components/Navbar';
+import { CardColumns } from 'react-bootstrap';
+
 
 function App() {
   return (
     <>
- 
-<Navbar />
+ <MyNavbar />
 <Navigation />
+<Carousls />
+   <div className='container'>
+      <h1>Hello Saud khan!</h1>
+      <br />
+    <p>lag lag ba kege tadi mko khan</p>
+    </div>
 
-<h1>Hello world!</h1>
+
+
 </>
   );
 }
